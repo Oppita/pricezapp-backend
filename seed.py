7d53edadbca7f038027db77234cbd56ae858967f
Oppita <opapaita8@gmail.com>
2025-10-07 09:22:14 -0500

    Update seed.py

--- seed.py
+++ seed.py
@@ -1,20 +1,21 @@
-from db import SessionLocal
+# seed.py
+from db import SessionLocal, engine, Base
 from models import Category, Product, User
 from passlib.context import CryptContext
 
 pwd = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 def run():
+    Base.metadata.create_all(bind=engine)
     db = SessionLocal()
-    # create categories if none
     if db.query(Category).count() == 0:
         c1 = Category(name='Almacén', icon='package', color='text-purple-700')
         c2 = Category(name='Bebidas', icon='cup-soda', color='text-purple-700')
         db.add_all([c1,c2]); db.commit()
-    # create admin user
     if db.query(User).filter(User.email=='admin@example.com').count() == 0:
         user = User(name='Admin', email='admin@example.com', hashed_password=pwd.hash('password123'), is_admin=True)
         db.add(user); db.commit()
+    db.close()
     print('Seed complete')
 
 if __name__ == '__main__':
